58c820af242ff19e4bbee078fabe55ed796a4e4d
MattPoly <matthieu.cayet@etu.univ-lyon1.fr>
2017-12-14 16:28:06 +0100

    Generate Fake datas

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+var fakeData = require('../services/fakedata');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.send('api works');
+  res.send(fakeData.getData())
 });
 
 module.exports = router;
---
+++ server/services/fakedata.js
@@ -0,0 +1,49 @@
+'use strict';
+// var Promise = require('bluebird');
+// var Rates = Promise.promisifyAll(require('../database/rates'));
+
+// Generate fake data for Stefano and Cyprien <3
+exports.getData = function() {
+  var data = [
+    {
+      "num_acc" : 1,
+      "mois" : 1,
+      "jour" : 1,
+      "hrmn" : 1,
+      "lum" : 1,
+      "isAgglo" : 1,
+      "inter" : 1,
+      "atm" : 1,
+      "col" : 1,
+      "com" : 1,
+      "dep" : 1
+    },
+    {
+      "num_acc" : 1,
+      "mois" : 1,
+      "jour" : 1,
+      "hrmn" : 1,
+      "lum" : 1,
+      "isAgglo" : 1,
+      "inter" : 1,
+      "atm" : 1,
+      "col" : 1,
+      "com" : 1,
+      "dep" : 1
+    },
+    {
+      "num_acc" : 1,
+      "mois" : 1,
+      "jour" : 1,
+      "hrmn" : 1,
+      "lum" : 1,
+      "isAgglo" : 1,
+      "inter" : 1,
+      "atm" : 1,
+      "col" : 1,
+      "com" : 1,
+      "dep" : 1
+    }
+  ];
+  return data;
+};
