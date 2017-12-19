7c8a0ed363a8ff0e97a2d0632db82a3de258a9af
CyprienF <floquet.cyprien@gmail.com>
2017-12-19 16:07:43 +0100

    Chart

--- src/app/chart-years/chart-years.component.ts
+++ src/app/chart-years/chart-years.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chart-years',
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartYearsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
